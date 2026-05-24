fd5bc5a603312da66fd3776f5ba4c1c1127d0644
swish <swish@example.com>
2026-05-24 19:49:36 +0800

    chore: 版本升级至 v2026.05.23，更新致谢信息
    版本号: 2026.05.13 → 2026.05.23
    致谢: 更正 Crystalxp 为代码贡献者身份
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/__version__.py
+++ src/__version__.py
@@ -3,14 +3,14 @@
 
 版权所有 (C) 吉林省左右软件开发有限公司
 Copyright (C) Equilibrium Software Development Co., Ltd, Jilin
-Update & Mod By Crystalxp (黑夜杀手 QQ:281309196)
+致谢: 感谢 Crystalxp (黑夜杀手 QQ:281309196) 的代码贡献，已合并入项目
 
-版本号: 2026.05.13
-发布日期: 2026-05-13
+版本号: 2026.05.23
+发布日期: 2026-05-24
 """
 
-__version__ = "2026.05.13"
-__release_date__ = "2026-05-13"
+__version__ = "2026.05.23"
+__release_date__ = "2026-05-24"
 __author__ = "吉林省左右软件开发有限公司"
 __copyright__ = "Copyright (C) 2026 吉林省左右软件开发有限公司 / Equilibrium Software Development Co., Ltd, Jilin"
 __license__ = "MIT"
